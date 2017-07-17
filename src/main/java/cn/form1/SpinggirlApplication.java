ec0ab64f66e949fec77eb17db4d14eb021c77a79
zjwlgr <zjwlgr@yeah.net>
2017-07-17 20:23:54 +0800

    first commit

---
+++ src/main/java/cn/form1/SpinggirlApplication.java
@@ -0,0 +1,12 @@
+package cn.form1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpinggirlApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpinggirlApplication.class, args);
+	}
+}
