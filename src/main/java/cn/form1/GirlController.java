67bd55f834e1947b2ececa0c2413a0f62cba55c7
zjwlgr <zjwlgr@yeah.net>
2017-07-17 21:55:56 +0800

    up

--- src/main/java/cn/form1/GirlController.java
+++ src/main/java/cn/form1/GirlController.java
@@ -1,15 +1,33 @@
 package cn.form1;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 
 @RestController
 public class GirlController {
 
-    @GetMapping(value = "/")
-    public String index(){
+    //在类中加载单一配置的值
+    @Value("${webname}")
+    private String webname;
 
-        return "index";
+    //引入注入配置的类
+    @Autowired
+    private Girlproperties girlproperties;
+
+    /*
+    * 测试获取配置文件中的信息
+    * */
+    @RequestMapping(value = "/config", method = RequestMethod.GET)
+    public String config(){
+        String str = this.webname + "_";
+        str += this.girlproperties.getCookietime() + "_";
+        str += this.girlproperties.getUserhomeurl() + "_";
+        str += this.girlproperties.getStartweb() + "_";
+        return str;
     }
 }
---
+++ src/main/java/cn/form1/Girlproperties.java
@@ -0,0 +1,44 @@
+package cn.form1;
+
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.stereotype.Component;
+
+/**
+ * 配置类
+ */
+
+@Component
+@ConfigurationProperties(prefix = "myconfig")
+//以上两个注解表示注入配置要加的注解
+public class Girlproperties {
+
+    private Integer cookietime;
+
+    private String userhomeurl;
+
+    private Integer startweb;
+
+    public Integer getCookietime() {
+        return cookietime;
+    }
+
+    public void setCookietime(Integer cookietime) {
+        this.cookietime = cookietime;
+    }
+
+    public String getUserhomeurl() {
+        return userhomeurl;
+    }
+
+    public void setUserhomeurl(String userhomeurl) {
+        this.userhomeurl = userhomeurl;
+    }
+
+    public Integer getStartweb() {
+        return startweb;
+    }
+
+    public void setStartweb(Integer startweb) {
+        this.startweb = startweb;
+    }
+}
