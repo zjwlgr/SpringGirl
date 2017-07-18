362b7af841ac7e9bf94a44c0538109a4bf35edc9
zjwlgr <zhangjian@kuaxue.com>
2017-07-18 11:21:30 +0800

    第一阶段代码完成提交

--- src/main/java/cn/form1/GirlController.java
+++ src/main/java/cn/form1/GirlController.java
@@ -2,10 +2,9 @@ package cn.form1;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 
 @RestController
@@ -19,6 +18,14 @@ public class GirlController {
     @Autowired
     private Girlproperties girlproperties;
 
+    //引入Repository接口类
+    @Autowired
+    private GirlRepository girlRepository;
+
+    //引入Service数据处理类
+    @Autowired
+    private GirlService girlService;
+
     /*
     * 测试获取配置文件中的信息
     * */
@@ -29,5 +36,64 @@ public class GirlController {
         str += this.girlproperties.getUserhomeurl() + "_";
         str += this.girlproperties.getStartweb() + "_";
         return str;
+    }
+
+    /*
+    * 查询所有Girl表中的信息
+    * */
+    @GetMapping(value = "/girl")
+    public List<Girl> listAll(){
+        return girlRepository.findAll();
+    }
+
+    /*
+    * 查询某一条信息
+    * */
+    @GetMapping(value = "/girl/{id}")
+    public Girl listOne(@PathVariable(value = "id") Integer id){
+        return girlRepository.findOne(id);
+    }
+
+    /*
+    * 添加一条信息
+    * */
+    @PostMapping(value = "/girl")
+    public Girl insertOne(@RequestParam(value = "cupsize",required = false,defaultValue = "F") String cupsize,
+                          @RequestParam(value = "age",required = true) Integer age){
+        Girl girl = new Girl();
+        girl.setAge(age);
+        girl.setCupSize(cupsize);
+        return girlRepository.save(girl);
+    }
+
+    /*
+    * 修改一条信息
+    * */
+    @PutMapping(value = "/girl")
+    public Girl updateOne(@RequestParam(value = "id",required = true) Integer id,
+                          @RequestParam(value = "cupsize",required = false,defaultValue = "F") String cupsize,
+                          @RequestParam(value = "age",required = true) Integer age){
+        Girl girl = new Girl();
+        girl.setId(id);
+        girl.setAge(age);
+        girl.setCupSize(cupsize);
+        return girlRepository.save(girl);
+    }
+
+    /*
+    * 删除一条信息
+    * */
+    @DeleteMapping(value = "/girl")
+    public Integer deleteOne(@RequestParam(value = "id",required = true) Integer id){
+        girlRepository.delete(id);
+        return 1;
+    }
+
+    /*
+    * 测试事务
+    * */
+    @GetMapping(value = "/tran")
+    public List<Girl> transactional(){
+        return girlService.insertAll();
     }
 }
---
+++ src/main/java/cn/form1/GirlService.java
@@ -0,0 +1,34 @@
+package cn.form1;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * 测试事务
+ */
+@Service
+public class GirlService {
+
+    @Autowired
+    private GirlRepository girlRepository;
+
+    @Transactional //表示事务，其中save全部成功或全部失败
+    public List<Girl> insertAll(){
+        List<Girl> list = new ArrayList<Girl>();
+        Girl girl1 = new Girl();
+        girl1.setCupSize("C");
+        girl1.setAge(26);
+        list.add(girlRepository.save(girl1));
+
+        Girl girl2 = new Girl();
+        girl2.setCupSize("A");
+        girl2.setAge(19);
+        list.add(girlRepository.save(girl2));
+        return list;
+    }
+
+}
