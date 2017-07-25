413d8a2d360276539183a74a6558c1e69e61ed62
zjwlgr <zjwlgr@yeah.net>
2017-07-25 23:08:35 +0800

    有思路了

--- src/main/java/cn/form1/interceptor/ErrorPageConfig.java
+++ src/main/java/cn/form1/interceptor/ErrorPageConfig.java
@@ -9,7 +9,7 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpStatus;
 
 
-@Configuration
+//@Configuration
 public class ErrorPageConfig implements EmbeddedServletContainerCustomizer {
 
     @Override
